47bfad921dc2eac3b8ffa1caf9a673988b728b57
xxss0903 <xxss0903@gmail.com>
2024-09-26 17:53:57 +0800

    feat: 修改打包

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/drawstamputils/'  // Add this line
+  base: './'
 })
