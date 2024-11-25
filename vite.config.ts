b57b24d286a3151c7fe32bf74c81893b06ce6b45
xxss0903 <xxss0903@gmail.com>
2024-11-25 23:59:48 +0800

    chore: Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: '/drawstamputils/',
   build: {
     outDir: "dist"
   }
