07e7c85044fa6e972e8ccbc6b80fd01c79b3ad30
xxss0903 <353768356@qq.com>
2024-12-19 23:20:49 +0800

    chore: 修改logo和标题

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/drawstamputils/',
+  base: '/',
   build: {
-    outDir: "docs"
+    outDir: "dist"
   }
 })
