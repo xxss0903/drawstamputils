b8848ebc111c0c6182ec457e5bd50dded7648738
xxss0903 <353768356@qq.com>
2024-12-19 23:25:26 +0800

    chore: 修改编译的build文件夹

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: '/drawstamputils/',
   build: {
-    outDir: "dist"
+    outDir: "docs"
   }
 })
+
