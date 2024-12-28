c1f70561655a4d9e55905464905307b2803df5ba
xxss0903 <353768356@qq.com>
2024-12-28 13:46:57 +0800

    chore: 删除多余的src文件

--- src/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './style.css'
-import i18n from './i18n'
-
-const app = createApp(App)
-app.use(i18n)
-app.mount('#app')
--- src/src/utils/DrawBorderUtils.ts
+++
@@ -1,8 +0,0 @@
-export function drawBasicBorder(ctx: CanvasRenderingContext2D, centerX: number, centerY: number, radiusX: number, radiusY: number, borderWidth: number, borderColor: string) {
-    ctx.beginPath();
-    ctx.ellipse(centerX, centerY, radiusX, radiusY, 0, 0, Math.PI * 2);
-    ctx.strokeStyle = borderColor;
-    ctx.lineWidth = borderWidth;
-    ctx.stroke();
-}
-
